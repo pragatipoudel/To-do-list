d7440786c9218b2f6543090ef8dc59ec4fc6be9b
Pragati Poudel <pragati.poudel37@gmail.com>
2021-10-24 18:34:23 -0400

    Intial commit

---
+++ main.js
@@ -0,0 +1,70 @@
+///State
+
+let items = [
+    {
+        text: 'Get Groceries',
+        checked: false,
+    },
+    {
+        text: 'Check email',
+        checked: true,
+    },
+    {
+        text: 'Water plants',
+        checked: false,
+    },
+];
+
+function save(){
+    let str = JSON.stringify(items);
+    localStorage.setItem('items', str);
+
+}
+function load(){
+    let str = localStorage.getItem('items');
+    if (str){
+     items = JSON.parse(str);
+    }
+}
+
+
+///Display
+let ul = document.getElementById("to-do-list");
+
+function display() {
+    ul.innerText = "";
+    items.forEach(item => {
+        let li = document.createElement('li');
+        ul.appendChild(li);
+
+        li.innerText = item.text;
+
+        let input = document.createElement('input');
+        input.type = 'checkbox';
+        input.checked = item.checked; 
+        input.onclick = () => {
+            item.checked = !item.checked;
+            display();
+        };
+        if (item.checked){
+            li.classList.add('checked');
+        }
+        li.prepend(input);
+    })
+    save();
+}
+
+
+let newInput = document.getElementById('new-item');
+newInput.onkeyup = function(event) {
+    if (event.key == 'Enter'){
+        items.push({
+            text: newInput.value,
+            checked: false
+        })
+        display();
+    }
+}
+
+load();
+display();
